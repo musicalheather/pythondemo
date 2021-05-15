5ea74384cf6ff62c5ebb6456e360244c1aa89219
Heather Bowes <musicalheather@gmail.com>
2021-05-15 18:47:19 -0400

    fixed urls

--- mysite/tempconvert/urls.py
+++ mysite/tempconvert/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+
 from . import views
 
 urlpatterns = [
-    path('tempconvert/', include('tempconvert.urls')),
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
