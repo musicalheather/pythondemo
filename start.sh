fbf53a1d03f97477cc9853c3b62d239223f29218
Heather Bowes <musicalheather@gmail.com>
2021-05-17 11:14:00 -0400

    hopefully fix postgres

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ printenv | sed 's/^\(.*\)$/export \1/g' > /app/project_env.sh
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py load_data
-python3 manage.py runserver 0.0.0.0:8080
+python3 manage.py runserver 0.0.0.0:8000
 #hello test
