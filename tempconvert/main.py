6388f070ff6dc3a28a3c2d8a3c78e198f380bbbb
Heather Bowes <musicalheather@gmail.com>
2021-06-01 10:03:01 -0400

    added kelvin

--- tempconvert/main.py
+++ tempconvert/main.py
@@ -7,21 +7,33 @@ app = Flask(__name__)
 def index():
 # Celsius to Fahrenheit
     celsius = request.args.get("celsius", "")
+    fahrenheit = request.args.get("fahrenheit", "")
+    kelvin = request.args.get("kelvin", "")
     if celsius:
         fahrenheit = fahrenheit_from(celsius)
-    else:
-        fahrenheit = ""
+    if fahrenheit:
+        celsius = celsius_from(fahrenheit)
+    if kelvin:
+        kelvin = kelvin_from(celsius)
+
     return (
         """<form action="" method="get">
                 Celsius temperature: <input type="text" name="celsius">
-                <input type="submit" value="Convert to Fahrenheit">
+                <input type="submit" value="Convert to Fahrenheit"><br>
                 Fahrenheit temperature: <input type="text" name="fahrenheit">
-                <input type="submit" value="Convert to Celsius">
+                <input type="submit" value="Convert to Celsius"><br>
+                Kelvin temperature: <input type="text" name="Kelvin">
+                <input type="submit" value="Convert to Kelvin"><br>
             </form>"""
         + "Fahrenheit: "
         + fahrenheit
+        + "<br>"
         + "Celsius: "
         + celsius
+        + "<br>"
+        + "Kelvin: "
+        + kelvin
+        + "<br>"
     )
 
 def fahrenheit_from(celsius):
@@ -36,9 +48,17 @@ def fahrenheit_from(celsius):
 def celsius_from(fahrenheit):
     """Convert Fahrenheit to celsius degrees."""
     try:
-        celsius = float(fahrenheit) * 9 / 5 + 32
+        celsius = float(fahrenheit) * 5 / 9 - 32
         celsius = round(celsius, 3)  # Round to three decimal places
         return str(celsius)
+    except ValueError:
+        return "invalid input"
+def kelvin_from(celsius):
+    """Convert celsius to kelvin degrees."""
+    try:
+        kelvin = float(celsius) + 273.15
+        kelvin = round(kelvin, 3)  # Round to three decimal places
+        return str(kelvin)
     except ValueError:
         return "invalid input"
 
