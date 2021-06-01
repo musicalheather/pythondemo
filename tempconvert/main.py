1aad5d6555df3164757ef85f8cf454b59ecd92ba
Heather Bowes <musicalheather@gmail.com>
2021-06-01 09:16:05 -0400

    playing with html

--- tempconvert/main.py
+++ tempconvert/main.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+# Celsius to Fahrenheit
     celsius = request.args.get("celsius", "")
     if celsius:
         fahrenheit = fahrenheit_from(celsius)
@@ -14,9 +15,13 @@ def index():
         """<form action="" method="get">
                 Celsius temperature: <input type="text" name="celsius">
                 <input type="submit" value="Convert to Fahrenheit">
+                Fahrenheit temperature: <input type="text" name="fahrenheit">
+                <input type="submit" value="Convert to Celsius">
             </form>"""
         + "Fahrenheit: "
         + fahrenheit
+        + "Celsius: "
+        + celsius
     )
 
 def fahrenheit_from(celsius):
@@ -25,6 +30,15 @@ def fahrenheit_from(celsius):
         fahrenheit = float(celsius) * 9 / 5 + 32
         fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
         return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
+# Fahrenheit to Celsius
+def celsius_from(fahrenheit):
+    """Convert Fahrenheit to celsius degrees."""
+    try:
+        celsius = float(fahrenheit) * 9 / 5 + 32
+        celsius = round(celsius, 3)  # Round to three decimal places
+        return str(celsius)
     except ValueError:
         return "invalid input"
 
