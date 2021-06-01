c66a9c27abaab3771fdd1c3d3e2352568a951e6f
Heather Bowes <musicalheather@gmail.com>
2021-06-01 17:37:18 -0400

    fixed dockerfile

--- tempconvert/main.py
+++ tempconvert/main.py
@@ -44,5 +44,5 @@ def index():
         + "<br>"
     )
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=8080, debug=False)
